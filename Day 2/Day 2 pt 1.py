bf3cc23fec18627751644abc99f0aea519de45d8
Emil280 <kondrackie28@gmail.com>
2022-12-07 16:52:29 +0000

    Day 2 Pt 1 Completed succsefully

--- Day 2/Day 2 pt 1.py
+++ Day 2/Day 2 pt 1.py
@@ -1,20 +1,19 @@
 Input=open("Day 2\stratergy guide.txt" , "r")
-Totaled=False
-Index=1
+Input=Input.readlines()
 Score=0
-while not Totaled:
-    line=Input.readline()
+for n in range (len(Input)):
+    line=Input[n]
     if "X" in line:
         Score+=1
         if "A" in line:
             Score+=3
-        elif "C":
+        elif "C" in line:
             Score+=6
     elif "Y" in line:
         Score+=2
         if "A" in line:
             Score+=6
-        elif "B":
+        elif "B" in line:
             Score+=3
     elif "Z" in line:
         Score+=3
@@ -22,7 +21,4 @@ while not Totaled:
             Score+=6
         elif "C" in line:
             Score+=3
-    Index+=1
-    if Index==len(Input.readlines()):
-        Totaled=True
 print(Score)
