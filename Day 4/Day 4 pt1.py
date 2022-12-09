84ea7df9db41f7e16b653d5583db6d6b061057d3
Emil280 <kondrackie28@gmail.com>
2022-12-09 21:58:09 +0000

    Day 4 pt 1 working

--- Day 4/Day 4 pt1.py
+++ Day 4/Day 4 pt1.py
@@ -8,48 +8,17 @@ for Pair in Pairs:
     Elf_2=Seperated_Pair[1]
     Elf_1_range=Elf_1.split('-')
     Elf_2_range=Elf_2.split('-')
-    Elf_1_start=Elf_1_range[0]
-    Elf_1_end=Elf_1_range[1]
-    Elf_2_start=Elf_2_range[0]
-    Elf_2_end=Elf_2_range[1]
-    if Elf_1_start < Elf_2_start and Elf_1_end > Elf_2_end:
+    Elf_1=[]
+    Elf_2=[]
+    for x in range(int(Elf_1_range[0]),int(Elf_1_range[1])+1):
+        Elf_1.append(x)
+    for x in range(int(Elf_2_range[0]),int(Elf_2_range[1])+1):
+        Elf_2.append(x)
+    if Elf_2[0] in Elf_1 and Elf_2[-1] in Elf_1:
         Overlaps+=1
-        print(f"Overlap in pair: {Pair}")
-    elif Elf_2_start < Elf_1_start and Elf_2_end > Elf_1_end:
+    elif Elf_1[0] in Elf_2 and Elf_1[-1] in Elf_2:
         Overlaps+=1
-        print(f"Overlap in pair: {Pair}")
-    elif Elf_1_start == Elf_1_end:
-        if Elf_1_start > Elf_2_start:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-        elif Elf_1_start == Elf_2_start:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-        elif Elf_1_end == Elf_2_end:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-    elif Elf_2_start == Elf_2_end:
-        if Elf_2_start > Elf_1_start:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-        elif Elf_2_start == Elf_1_start:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-        elif Elf_2_end == Elf_1_end:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-    elif Elf_1_start == Elf_2_start:
-        if Elf_1_end > Elf_2_end:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-        elif Elf_2_end > Elf_1_end:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-    elif Elf_1_end == Elf_2_end:
-        if Elf_1_start < Elf_2_start:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
-        elif Elf_2_start < Elf_1_start:
-            Overlaps+=1
-            print(f"Overlap in pair: {Pair}")
+    else:
+        pass
+    
 print(Overlaps)
